7b3d8df58d6f116f703d80cadc94c5093f1e5ebf
Leonardo Igor De Oliveira Cavalcante <bufige1434@gmail.com>
2020-03-22 07:44:41 -0300

    Updates

---
+++ precache-manifest.dc30bc00d5c0c6fca1ec91d860cc7718.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1de882e983dd3cb9a49c35be180c77",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "84321760e68ab3b03670",
+    "url": "/portfolio/static/js/2.ae49a4de.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.ae49a4de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd485d61a88f678d116",
+    "url": "/portfolio/static/js/main.42dc6852.chunk.js"
+  },
+  {
+    "revision": "6eac1c87e9dd7f30f4e9",
+    "url": "/portfolio/static/js/runtime-main.e43fef70.js"
+  }
+]);
